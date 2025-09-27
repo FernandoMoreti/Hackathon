6911db8a9fdff0025a8b1ab098c90b6d0c4cd9ef
Fernando Moreti <fernandombolela@gmail.com>
2025-09-27 19:15:51 -0300

    Validando backend

--- apps/Backend/src/routes/index.js
+++ apps/Backend/src/routes/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const { createBeneficiario } = require("../Controllers/UserController");
+const { especialidade } = require("../Controllers/EspecialidadeController");
 const router = express.Router();
 
 router.get("/beneficiario/", createBeneficiario)
+router.get("/especialidade/", especialidade)
 
 module.exports = router;
